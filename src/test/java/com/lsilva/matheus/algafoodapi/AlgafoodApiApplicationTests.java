e3bdcccc047f9f07f16919cb6ce8a6ab12ce23e9
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 10:45:15 -0300

    criando projeto

---
+++ src/test/java/com/lsilva/matheus/algafoodapi/AlgafoodApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lsilva.matheus.algafoodapi;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AlgafoodApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
