6b6be9ac6d4266fb399fc1332d39b3871ae2cf7f
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 15:25:06 -0300

    Criando o primeiro controller.

---
+++ src/main/java/com/lsilva/matheus/algafoodapi/MeuPrimeiroController.java
@@ -0,0 +1,14 @@
+package com.lsilva.matheus.algafoodapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello!";
+    }
+}
