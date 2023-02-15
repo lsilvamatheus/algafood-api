1d4c122e2dc652a99371182f4073b955e67377ed
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-15 13:20:57 -0300

    Alterando classe NotificadorEmail para implementar interface.

--- src/main/java/com/lsilva/matheus/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/lsilva/matheus/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -5,11 +5,16 @@ import com.lsilva.matheus.algafoodapi.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
 
+    public NotificadorEmail() {
+        System.out.println("NotificadorEmail");
+    }
+
+
+    @Override
     public void notificar(Cliente cliente, String mensagem) {
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
         cliente.getNome(), cliente.getEmail(), mensagem);
-
     }
 }
